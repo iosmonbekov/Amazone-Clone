f7f8e3f13df9b2dcf4617e058b63814c8ea23c49
iosmonbekov <iosmonbekov666@gmail.com>
2021-02-06 17:15:52 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export default  {
+    products: [
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: './images/slimshirt.webp',
+            price: 30,
+            brand: 'Nike',
+            rating: 4.7,
+            numReviews: 25
+        },
+        {
+            name: 'Cool Shirt',
+            image: 'https://i.pinimg.com/originals/a3/b0/cf/a3b0cf3f26cd1dbcca84e5a80299624e.jpg',
+            category: 'Shirts',
+            price: 50,
+            brand: 'Prada',
+            rating: 4.1,
+            numReviews: 5
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: 'https://ridestore.imgix.net/catalog/product/E/7/E7779_1.jpg?w=320&auto=format&q=50',
+            price: 40,
+            brand: 'Adidas',
+            rating: 4.4,
+            numReviews: 106
+        },
+    ]
+}
